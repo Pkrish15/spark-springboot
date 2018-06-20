9186f96a29c5875cd2d937ee7b32e2f0881da5c1
root <root@localhost>
2018-06-20 14:30:23 +0530

    Initial Commit

---
+++ Downloads/spring-spark-example-master/src/main/java/com/redhat/gpte/ApplicationConfig.java
@@ -0,0 +1,57 @@
+package com.redhat.gpte;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+/**
+ * Created by Prakrish
+ */
+@Configuration
+@PropertySource("classpath:application.properties")
+public class ApplicationConfig {
+
+    @Value("${app.name:spark-boot}")
+    private String appName;
+
+    @Value("${spark.home}")
+    private String sparkHome;
+
+    @Value("${master.uri:local}")
+    private String masterUri;
+
+    @Bean
+    public SparkConf sparkConf() {
+        SparkConf sparkConf = new SparkConf()
+                .setAppName(appName)
+                .setSparkHome(sparkHome)
+                .setMaster(masterUri);
+
+        return sparkConf;
+    }
+
+    @Bean
+    public JavaSparkContext javaSparkContext() {
+        return new JavaSparkContext(sparkConf());
+    }
+
+    @Bean
+    public SparkSession sparkSession() {
+        return SparkSession
+                .builder()
+                .sparkContext(javaSparkContext().sc())
+                .appName("Java Spark SQL basic example")
+                .getOrCreate();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
---
+++ Downloads/spring-spark-example-master/src/main/java/com/redhat/gpte/SparkExperimentApplication.java
@@ -0,0 +1,12 @@
+package com.redhat.gpte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkExperimentApplication.class, args);
+	}
+}
